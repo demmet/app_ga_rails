b85d67b70f421acabbab507adf647423a3bd80ad
Demetrius Manica <demetrius.manica@ateliware.com.br>
2018-01-19 15:24:40 -0200

    Fixing initializer commented line

--- config/initializers/load_repositories.rb
+++ config/initializers/load_repositories.rb
@@ -5,7 +5,7 @@ begin
 	puts "Loading repositories... This will take a few seconds..."
 	languages = [:ruby, :c, :java, :php, :assembly]
 	create_languages(languages)
-	#load_all_repositories(Language.all())
+	load_all_repositories(Language.all())
 	puts "Repositories loaded... "
 rescue StandardError => e
 end
